d1be29e913aedca3f6aa1d9af01eb21edd942db9
Vipin Kumar <resetpa7@gmail.com>
2021-07-23 14:31:58 +0530

    portfolio updated

---
+++ myapp/modules/subscriptionschema.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+require('dotenv').config();
+var uri = process.env.DATABASEADMIN
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false,
+useCreateIndex: true});
+var conn = mongoose.Collection;
+
+var subscribeSchema = new mongoose.Schema({
+
+SubscriptionEmail: {
+    type: String,
+    required: true
+    
+},
+Date: {
+    type: Date,
+    default: Date.now
+}
+});
+
+var subscribeModel = mongoose.model('subscriptions', subscribeSchema);
+module.exports = subscribeModel;
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,16 +2,23 @@
 var express = require('express');
 var router = express.Router();
 var contactUsModel = require('../modules/contactusschema');
+var subscribeModel = require('../modules/subscriptionschema');
 // GET home page. */ // exactly correct one
 router.get('/', function(req, res, next) {
   
-  res.render('index', { title: 'Quick Website', msg:''});
+  res.render('index', { title: 'Vipin Kumar Portfolio', msg:''});
 });
 
 // GET home page. */ // exactly correct one
 router.get('/contactus', function(req, res, next) {
   
-  res.render('index', { title: 'Quick Website', msg:''});
+  res.render('index', { title: 'Vipin Kumar Portfolio', msg:''});
+});
+
+// GET home page. */ // exactly correct one
+router.get('/subscribe', function(req, res, next) {
+  
+  res.render('index', { title: 'Vipin Kumar Portfolio', msg:''});
 });
 
 router.post('/contactus', function(req, res, next) {
@@ -24,9 +31,22 @@ router.post('/contactus', function(req, res, next) {
   });
   contactUsDetail.save((err) => {
     if(err) {
-      res.render('index', { title: 'Quick Website', msg:'Error Occurred, Please Try again...'});
+      res.render('index', { title: 'Vipin Kumar Portfolio', msg:'Error Occurred, Please Try again...'});
     }
-    res.render('index', { title: 'Quick Website', msg:'Message Submited Successfully, You will be contacted soon'});
+    res.render('index', { title: 'Vipin Kumar Portfolio', msg:'Message Submited Successfully, You will be contacted soon'});
+  });
+  
+});
+
+router.post('/subscribe', function(req, res, next) {
+  var subscriptionDetail = new subscribeModel({
+    SubscriptionEmail: req.body.email
+  });
+  subscriptionDetail.save((err) => {
+    if(err) {
+      res.render('index', { title: 'Vipin Kumar Portfolio', msg:'Error Occurred, Please Try again...'});
+    }
+    res.render('index', { title: 'Vipin Kumar Portfolio', msg:'Subscribed Successfully!'});
   });
   
 });
